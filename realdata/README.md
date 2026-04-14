6d4428631ec1da79c8f37b9e0f63eddb671ca980
JinyuanChang-Lab <168390468+JinyuanChang-Lab@users.noreply.github.com>
2026-04-14 17:15:05 +0800

    Add files via upload

---
+++ realdata/README.md
@@ -0,0 +1,12 @@
+## Data:
+
+PI_DATA.txt
+
+## To get the results in the paper:
+
+* Run realdata.R to get Figure 5 in the main text, and two additional real data figures (Figures F27 and F28) in the Supplementary Material.
+
+* The folder realdata_plot contains all subfigures of Figures 5, F27 and F28.
+
+## Note:
+* gm.R and Inverse_real.R are source files of realdata.R.
--- realdata/realdata.R
+++ realdata/realdata.R
@@ -1,12 +1,12 @@
-setwd("C:/Users/朱正天/Desktop/Knockoff_code/realdata")
-
 library(knockoff)
 library(glmnet)
 library(scalreg)
 library(doParallel)
 
 source("gm.R")
-source("Inverse_real.R")# 50 max iterations
+source("Inverse_real.R")
+
+set.seed(2024)
 
 #Test the PI drug
 drug_class = 'PI'
@@ -60,8 +60,8 @@ X = X[,colSums(X) != 0]
 Y = gene_df[,4:(pos_start-1)]
 
 
-# Compare 4 methods (M1 and M2 with asdp MX knockoffs, clime inverting; GM and fixed knockoff).
-fourmethods <- function (X, y, q) {
+# Comparison 
+Comparison <- function (X, y, q) {
   # Log-transform the drug resistance measurements.
   y = log(y)
   
@@ -76,8 +76,15 @@ fourmethods <- function (X, y, q) {
   # Remove duplicate predictors.
   X = X[,colSums(abs(cor(X)-1) < 1e-4) == 1]
   
+  # Original X and y
+  X0 = X
+  y0 = y
   
-  # Run Method1 and Method2 and MX knockoff
+  # Centering X and y
+  X = scale(X, center = TRUE, scale = FALSE)
+  y = scale(y, center = TRUE, scale = FALSE)
+  
+  # Run Algorithm 1 and Algorithm 2 and MX knockoff
   p = ncol(X)
   n = nrow(X)
   
@@ -125,35 +132,35 @@ fourmethods <- function (X, y, q) {
   M1_selected = colnames(X[ ,index2])
   
   # Knockoff-assisted BBH
-  p21[p1 >= sqrt(q)] = 1
+  p21[p1 > sqrt(q)] = 1
   p21a = p.adjust(p21, method = "BH")
   index21 = seq(along = p21a)[p21a <= sqrt(q)]
   M2_selected = colnames(X[ ,index21])
   
   # Run GM method
-  GM = gm(y, X, q = q)
+  GM = gm(y0, X0, q = q)
   GM_index = GM$gm_selected
-  GM_selected = colnames(X[ ,GM_index])
+  GM_selected = colnames(X0[ ,GM_index])
   
   # Run the knockoff filter.
   knock.gen = function(x) create.fixed(x, method='equi')
-  Fixresult = knockoff.filter(X, y, fdr=q, knockoffs=knock.gen, statistic=stat.glmnet_lambdasmax)
+  Fixresult = knockoff.filter(X0, y0, fdr=q, knockoffs=knock.gen, statistic=stat.glmnet_lambdasmax)
   knockoff_selected = names(Fixresult$selected)
   
   list(Method1 = M1_selected, Method2 = M2_selected, GM = GM_selected, Knockoff = knockoff_selected)
 }
 
-# Run all 7 responses
-# Test fdr = 0.1 
-fdr = 0.1
-results = lapply(Y, function(y) fourmethods(X, y, 0.1))
 
+
+# Run for all 7 responses
+# Test fdr = 0.1 
+results01 = lapply(Y, function(y) Comparison(X, y, 0.1))
 
 # Calculate TP FP and FDP
 get_position <- function(x)
   sapply(regmatches(x, regexpr('[[:digit:]]+', x)), as.numeric)
 
-comparisons <- lapply(results, function(drug) {
+comparisons01 <- lapply(results01, function(drug) {
   lapply(drug, function(selected) {
     positions = unique(get_position(selected)) # remove possible duplicates
     discoveries = length(positions)
@@ -165,9 +172,8 @@ comparisons <- lapply(results, function(drug) {
 })
 
 
-# Run all 7 responses
 # Test fdr = 0.2
-results02 = lapply(Y, function(y) fourmethods(X, y, 0.2))
+results02 = lapply(Y, function(y) Comparison(X, y, 0.2))
 comparisons02 <- lapply(results02, function(drug) {
   lapply(drug, function(selected) {
     positions = unique(get_position(selected)) # remove possible duplicates
@@ -179,9 +185,9 @@ comparisons02 <- lapply(results02, function(drug) {
   })
 })
 
-# Run all 7 responses
+
 # Test fdr = 0.05
-results005 = lapply(Y, function(y) fourmethods(X, y, 0.05))
+results005 = lapply(Y, function(y) Comparison(X, y, 0.05))
 comparisons005 <- lapply(results005, function(drug) {
   lapply(drug, function(selected) {
     positions = unique(get_position(selected)) # remove possible duplicates
@@ -193,3 +199,227 @@ comparisons005 <- lapply(results005, function(drug) {
   })
 })
 
+
+###################################################################
+
+cnames = c("Algorithm1", "Algorithm2", "GM", "Knockoff", "B-BH")
+rnames = c("true_discoveries", "false_discoveries")
+
+#For BBH, we directly extract the data according to Sarkar and Tang (2022).
+
+
+######### APV 0.05
+apv005 = matrix(c(comparisons005$APV$Method1$true_discoveries, comparisons005$APV$Method1$false_discoveries,
+                  comparisons005$APV$Method2$true_discoveries, comparisons005$APV$Method2$false_discoveries,
+                  comparisons005$APV$GM$true_discoveries, comparisons005$APV$GM$false_discoveries,
+                  comparisons005$APV$Knockoff$true_discoveries, comparisons005$APV$Knockoff$false_discoveries,
+                  6, 0), nrow = 2, ncol = 5, byrow = FALSE, dimnames = list(rnames, cnames))
+
+pdf("./realdata_plot/apv005.pdf", width = 10, height = 8)
+barplot(apv005, main = "Resistance to APV", 
+        col = c('navy', 'orange'), ylim = c(0, 40), cex.axis = 2, cex.main = 2, cex.names = 1.5)
+dev.off()
+
+######### ATV 0.05
+atv005 = matrix(c(comparisons005$ATV$Method1$true_discoveries, comparisons005$ATV$Method1$false_discoveries,
+                  comparisons005$ATV$Method2$true_discoveries, comparisons005$ATV$Method2$false_discoveries,
+                  comparisons005$ATV$GM$true_discoveries, comparisons005$ATV$GM$false_discoveries,
+                  comparisons005$ATV$Knockoff$true_discoveries, comparisons005$ATV$Knockoff$false_discoveries,
+                  0, 0), nrow = 2, ncol = 5, byrow = FALSE, dimnames = list(rnames, cnames))
+
+pdf("./realdata_plot/atv005.pdf", width = 10, height = 8)
+barplot(atv005, main = "Resistance to ATV", 
+        col = c('navy', 'orange'), ylim = c(0, 40), cex.axis = 2, cex.main = 2, cex.names = 1.5)
+dev.off()
+
+######### IDV 0.05
+idv005 = matrix(c(comparisons005$IDV$Method1$true_discoveries, comparisons005$IDV$Method1$false_discoveries,
+                  comparisons005$IDV$Method2$true_discoveries, comparisons005$IDV$Method2$false_discoveries,
+                  comparisons005$IDV$GM$true_discoveries, comparisons005$IDV$GM$false_discoveries,
+                  comparisons005$IDV$Knockoff$true_discoveries, comparisons005$IDV$Knockoff$false_discoveries,
+                  12, 0), nrow = 2, ncol = 5, byrow = FALSE, dimnames = list(rnames, cnames))
+
+pdf("./realdata_plot/idv005.pdf", width = 10, height = 8)
+barplot(idv005, main = "Resistance to IDV", 
+        col = c('navy', 'orange'), ylim = c(0, 40), cex.axis = 2, cex.main = 2, cex.names = 1.5)
+dev.off()
+
+######### LPV 0.05
+lpv005 = matrix(c(comparisons005$LPV$Method1$true_discoveries, comparisons005$LPV$Method1$false_discoveries,
+                  comparisons005$LPV$Method2$true_discoveries, comparisons005$LPV$Method2$false_discoveries,
+                  comparisons005$LPV$GM$true_discoveries, comparisons005$LPV$GM$false_discoveries,
+                  comparisons005$LPV$Knockoff$true_discoveries, comparisons005$LPV$Knockoff$false_discoveries,
+                  5, 0), nrow = 2, ncol = 5, byrow = FALSE, dimnames = list(rnames, cnames))
+
+pdf("./realdata_plot/lpv005.pdf", width = 10, height = 8)
+barplot(lpv005, main = "Resistance to LPV", 
+        col = c('navy', 'orange'), ylim = c(0, 40), cex.axis = 2, cex.main = 2, cex.names = 1.5)
+dev.off()
+
+######### NFV 0.05
+nfv005 = matrix(c(comparisons005$NFV$Method1$true_discoveries, comparisons005$NFV$Method1$false_discoveries,
+                  comparisons005$NFV$Method2$true_discoveries, comparisons005$NFV$Method2$false_discoveries,
+                  comparisons005$NFV$GM$true_discoveries, comparisons005$NFV$GM$false_discoveries,
+                  comparisons005$NFV$Knockoff$true_discoveries, comparisons005$NFV$Knockoff$false_discoveries,
+                  10, 1), nrow = 2, ncol = 5, byrow = FALSE, dimnames = list(rnames, cnames))
+
+pdf("./realdata_plot/nfv005.pdf", width = 10, height = 8)
+barplot(nfv005, main = "Resistance to NFV", 
+        col = c('navy', 'orange'), ylim = c(0, 40), cex.axis = 2, cex.main = 2, cex.names = 1.5)
+dev.off()
+
+######### RTV 0.05
+rtv005 = matrix(c(comparisons005$RTV$Method1$true_discoveries, comparisons005$RTV$Method1$false_discoveries,
+                  comparisons005$RTV$Method2$true_discoveries, comparisons005$RTV$Method2$false_discoveries,
+                  comparisons005$RTV$GM$true_discoveries, comparisons005$RTV$GM$false_discoveries,
+                  comparisons005$RTV$Knockoff$true_discoveries, comparisons005$RTV$Knockoff$false_discoveries,
+                  7, 1), nrow = 2, ncol = 5, byrow = FALSE, dimnames = list(rnames, cnames))
+
+pdf("./realdata_plot/rtv005.pdf", width = 10, height = 8)
+barplot(rtv005, main = "Resistance to RTV", 
+        col = c('navy', 'orange'), ylim = c(0, 40), cex.axis = 2, cex.main = 2, cex.names = 1.5)
+dev.off()
+
+######### APV 0.1
+apv01 = matrix(c(comparisons01$APV$Method1$true_discoveries, comparisons01$APV$Method1$false_discoveries,
+                 comparisons01$APV$Method2$true_discoveries, comparisons01$APV$Method2$false_discoveries,
+                 comparisons01$APV$GM$true_discoveries, comparisons01$APV$GM$false_discoveries,
+                 comparisons01$APV$Knockoff$true_discoveries, comparisons01$APV$Knockoff$false_discoveries,
+                 7, 1), nrow = 2, ncol = 5, byrow = FALSE, dimnames = list(rnames, cnames))
+
+pdf("./realdata_plot/apv01.pdf", width = 10, height = 8)
+barplot(apv01, main = "Resistance to APV", 
+        col = c('navy', 'orange'), ylim = c(0, 40), cex.axis = 2, cex.main = 2, cex.names = 1.5)
+dev.off()
+
+######### ATV 0.1
+atv01 = matrix(c(comparisons01$ATV$Method1$true_discoveries, comparisons01$ATV$Method1$false_discoveries,
+                 comparisons01$ATV$Method2$true_discoveries, comparisons01$ATV$Method2$false_discoveries,
+                 comparisons01$ATV$GM$true_discoveries, comparisons01$ATV$GM$false_discoveries,
+                 comparisons01$ATV$Knockoff$true_discoveries, comparisons01$ATV$Knockoff$false_discoveries,
+                 0, 0), nrow = 2, ncol = 5, byrow = FALSE, dimnames = list(rnames, cnames))
+
+pdf("./realdata_plot/atv01.pdf", width = 10, height = 8)
+barplot(atv01, main = "Resistance to ATV", 
+        col = c('navy', 'orange'), ylim = c(0, 40), cex.axis = 2, cex.main = 2, cex.names = 1.5)
+dev.off()
+
+######### IDV 0.1
+idv01 = matrix(c(comparisons01$IDV$Method1$true_discoveries, comparisons01$IDV$Method1$false_discoveries,
+                 comparisons01$IDV$Method2$true_discoveries, comparisons01$IDV$Method2$false_discoveries,
+                 comparisons01$IDV$GM$true_discoveries, comparisons01$IDV$GM$false_discoveries,
+                 comparisons01$IDV$Knockoff$true_discoveries, comparisons01$IDV$Knockoff$false_discoveries,
+                 14, 2), nrow = 2, ncol = 5, byrow = FALSE, dimnames = list(rnames, cnames))
+
+pdf("./realdata_plot/idv01.pdf", width = 10, height = 8)
+barplot(idv01, main = "Resistance to IDV", 
+        col = c('navy', 'orange'), ylim = c(0, 40), cex.axis = 2, cex.main = 2, cex.names = 1.5)
+dev.off()
+
+######### LPV 0.1
+lpv01 = matrix(c(comparisons01$LPV$Method1$true_discoveries, comparisons01$LPV$Method1$false_discoveries,
+                 comparisons01$LPV$Method2$true_discoveries, comparisons01$LPV$Method2$false_discoveries,
+                 comparisons01$LPV$GM$true_discoveries, comparisons01$LPV$GM$false_discoveries,
+                 comparisons01$LPV$Knockoff$true_discoveries, comparisons01$LPV$Knockoff$false_discoveries,
+                 5, 0), nrow = 2, ncol = 5, byrow = FALSE, dimnames = list(rnames, cnames))
+
+pdf("./realdata_plot/lpv01.pdf", width = 10, height = 8)
+barplot(lpv01, main = "Resistance to LPV", 
+        col = c('navy', 'orange'), ylim = c(0, 40), cex.axis = 2, cex.main = 2, cex.names = 1.5)
+dev.off()
+
+######### NFV 0.1
+nfv01 = matrix(c(comparisons01$NFV$Method1$true_discoveries, comparisons01$NFV$Method1$false_discoveries,
+                 comparisons01$NFV$Method2$true_discoveries, comparisons01$NFV$Method2$false_discoveries,
+                 comparisons01$NFV$GM$true_discoveries, comparisons01$NFV$GM$false_discoveries,
+                 comparisons01$NFV$Knockoff$true_discoveries, comparisons01$NFV$Knockoff$false_discoveries,
+                 12, 3), nrow = 2, ncol = 5, byrow = FALSE, dimnames = list(rnames, cnames))
+
+pdf("./realdata_plot/nfv01.pdf", width = 10, height = 8)
+barplot(nfv01, main = "Resistance to NFV", 
+        col = c('navy', 'orange'), ylim = c(0, 40), cex.axis = 2, cex.main = 2, cex.names = 1.5)
+dev.off()
+
+######### RTV 0.1
+rtv01 = matrix(c(comparisons01$RTV$Method1$true_discoveries, comparisons01$RTV$Method1$false_discoveries,
+                 comparisons01$RTV$Method2$true_discoveries, comparisons01$RTV$Method2$false_discoveries,
+                 comparisons01$RTV$GM$true_discoveries, comparisons01$RTV$GM$false_discoveries,
+                 comparisons01$RTV$Knockoff$true_discoveries, comparisons01$RTV$Knockoff$false_discoveries,
+                 9, 1), nrow = 2, ncol = 5, byrow = FALSE, dimnames = list(rnames, cnames))
+
+pdf("./realdata_plot/rtv01.pdf", width = 10, height = 8)
+barplot(rtv01, main = "Resistance to RTV", 
+        col = c('navy', 'orange'), ylim = c(0, 40), cex.axis = 2, cex.main = 2, cex.names = 1.5)
+dev.off()
+
+######### APV 0.2
+apv02 = matrix(c(comparisons02$APV$Method1$true_discoveries, comparisons02$APV$Method1$false_discoveries,
+                 comparisons02$APV$Method2$true_discoveries, comparisons02$APV$Method2$false_discoveries,
+                 comparisons02$APV$GM$true_discoveries, comparisons02$APV$GM$false_discoveries,
+                 comparisons02$APV$Knockoff$true_discoveries, comparisons02$APV$Knockoff$false_discoveries,
+                 9, 2), nrow = 2, ncol = 5, byrow = FALSE, dimnames = list(rnames, cnames))
+
+pdf("./realdata_plot/apv02.pdf", width = 10, height = 8)
+barplot(apv02, main = "Resistance to APV", 
+        col = c('navy', 'orange'), ylim = c(0, 40), cex.axis = 2, cex.main = 2, cex.names = 1.5)
+dev.off()
+
+######### ATV 0.2
+atv02 = matrix(c(comparisons02$ATV$Method1$true_discoveries, comparisons02$ATV$Method1$false_discoveries,
+                 comparisons02$ATV$Method2$true_discoveries, comparisons02$ATV$Method2$false_discoveries,
+                 comparisons02$ATV$GM$true_discoveries, comparisons02$ATV$GM$false_discoveries,
+                 comparisons02$ATV$Knockoff$true_discoveries, comparisons02$ATV$Knockoff$false_discoveries,
+                 0, 0), nrow = 2, ncol = 5, byrow = FALSE, dimnames = list(rnames, cnames))
+
+pdf("./realdata_plot/atv02.pdf", width = 10, height = 8)
+barplot(atv02, main = "Resistance to ATV", 
+        col = c('navy', 'orange'), ylim = c(0, 40), cex.axis = 2, cex.main = 2, cex.names = 1.5)
+dev.off()
+
+######### IDV 0.2
+idv02 = matrix(c(comparisons02$IDV$Method1$true_discoveries, comparisons02$IDV$Method1$false_discoveries,
+                 comparisons02$IDV$Method2$true_discoveries, comparisons02$IDV$Method2$false_discoveries,
+                 comparisons02$IDV$GM$true_discoveries, comparisons02$IDV$GM$false_discoveries,
+                 comparisons02$IDV$Knockoff$true_discoveries, comparisons02$IDV$Knockoff$false_discoveries,
+                 15, 4), nrow = 2, ncol = 5, byrow = FALSE, dimnames = list(rnames, cnames))
+
+pdf("./realdata_plot/idv02.pdf", width = 10, height = 8)
+barplot(idv02, main = "Resistance to IDV", 
+        col = c('navy', 'orange'), ylim = c(0, 40), cex.axis = 2, cex.main = 2, cex.names = 1.5)
+dev.off()
+
+######### LPV 0.2
+lpv02 = matrix(c(comparisons02$LPV$Method1$true_discoveries, comparisons02$LPV$Method1$false_discoveries,
+                 comparisons02$LPV$Method2$true_discoveries, comparisons02$LPV$Method2$false_discoveries,
+                 comparisons02$LPV$GM$true_discoveries, comparisons02$LPV$GM$false_discoveries,
+                 comparisons02$LPV$Knockoff$true_discoveries, comparisons02$LPV$Knockoff$false_discoveries,
+                 5, 0), nrow = 2, ncol = 5, byrow = FALSE, dimnames = list(rnames, cnames))
+
+pdf("./realdata_plot/lpv02.pdf", width = 10, height = 8)
+barplot(lpv02, main = "Resistance to LPV", 
+        col = c('navy', 'orange'), ylim = c(0, 40), cex.axis = 2, cex.main = 2, cex.names = 1.5)
+dev.off()
+
+######### NFV 0.2
+nfv02 = matrix(c(comparisons02$NFV$Method1$true_discoveries, comparisons02$NFV$Method1$false_discoveries,
+                 comparisons02$NFV$Method2$true_discoveries, comparisons02$NFV$Method2$false_discoveries,
+                 comparisons02$NFV$GM$true_discoveries, comparisons02$NFV$GM$false_discoveries,
+                 comparisons02$NFV$Knockoff$true_discoveries, comparisons02$NFV$Knockoff$false_discoveries,
+                 12, 6), nrow = 2, ncol = 5, byrow = FALSE, dimnames = list(rnames, cnames))
+
+pdf("./realdata_plot/nfv02.pdf", width = 10, height = 8)
+barplot(nfv02, main = "Resistance to NFV", 
+        col = c('navy', 'orange'), ylim = c(0, 40), cex.axis = 2, cex.main = 2, cex.names = 1.5)
+dev.off()
+
+######### RTV 0.2
+rtv02 = matrix(c(comparisons02$RTV$Method1$true_discoveries, comparisons02$RTV$Method1$false_discoveries,
+                 comparisons02$RTV$Method2$true_discoveries, comparisons02$RTV$Method2$false_discoveries,
+                 comparisons02$RTV$GM$true_discoveries, comparisons02$RTV$GM$false_discoveries,
+                 comparisons02$RTV$Knockoff$true_discoveries, comparisons02$RTV$Knockoff$false_discoveries,
+                 11, 2), nrow = 2, ncol = 5, byrow = FALSE, dimnames = list(rnames, cnames))
+
+pdf("./realdata_plot/rtv02.pdf", width = 10, height = 8)
+barplot(rtv02, main = "Resistance to RTV", 
+        col = c('navy', 'orange'), ylim = c(0, 40), cex.axis = 2, cex.main = 2, cex.names = 1.5)
+dev.off()
